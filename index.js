0a1b596025a93461590f1397f86a24d03eb63fa6
SAREMY-9 <brianowino226@gmail.com>
2025-03-11 17:27:30 +0300

    Revert "react interactivity"
    This reverts commit 2d5dd97996722308dbceb34d35758fcdc007b220.

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Services from "./react-components/Services.js";
-
-const root = createRoot(document.getElementById("services-root"));
-root.render(<Services />);
